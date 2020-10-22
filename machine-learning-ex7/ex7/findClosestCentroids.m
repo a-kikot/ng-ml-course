358d19977faca10508a076f61f3c6b26303bb40a
Anna.Kikot <ak.ildsj@gmail.com>
2020-10-22 21:00:41 +0300

    Complete the code

--- machine-learning-ex7/ex7/findClosestCentroids.m
+++ machine-learning-ex7/ex7/findClosestCentroids.m
@@ -11,23 +11,11 @@ K = size(centroids, 1);
 % You need to return the following variables correctly.
 idx = zeros(size(X,1), 1);
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Go over every example, find its closest centroid, and store
-%               the index inside idx at the appropriate location.
-%               Concretely, idx(i) should contain the index of the centroid
-%               closest to example i. Hence, it should be a value in the 
-%               range 1..K
-%
-% Note: You can use a for-loop over the examples to compute this.
-%
-
-
-
-
-
-
-
-% =============================================================
+for i = 1:length(X)
+	distances = sum((X(i, :) .- centroids) .^ 2, 2);
+	min_distance = min(distances);
+	idx(i) = find(distances == min_distance);
+end
 
 end
 
--- machine-learning-ex7/ex7/pca.m
+++ machine-learning-ex7/ex7/pca.m
@@ -11,21 +11,7 @@ function [U, S] = pca(X)
 U = zeros(n);
 S = zeros(n);
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: You should first compute the covariance matrix. Then, you
-%               should use the "svd" function to compute the eigenvectors
-%               and eigenvalues of the covariance matrix. 
-%
-% Note: When computing the covariance matrix, remember to divide by m (the
-%       number of examples).
-%
-
-
-
-
-
-
-
-% =========================================================================
+Sigma = X' * X / m;  % cov matrix 
+[U, S, V] = svd(Sigma);
 
 end
